295330e81dc779f37cd0ac84f791241e3b5fd8ac
Myroslav Kryvoruchko <90711690+kmirik999@users.noreply.github.com>
2022-05-10 22:02:33 +0300

    Implement preparation

---
+++ IT-4-term-Lab-7/Program.cs
@@ -0,0 +1,37 @@
+﻿var inputLine = Console.ReadLine();
+
+
+foreach (var separator in new string[]{",", "." ,";" ,":", "?", "!", "(", ")", "[", "]", "{", "}", "/"})
+{
+    inputLine = inputLine.Replace(separator, "");
+}
+
+var words = inputLine.Split(' ');
+
+Console.WriteLine(inputLine);
+
+var file = File.ReadLines("words_list.txt");
+var list = new List<string>();
+
+foreach (var line in file)
+{
+    list.Add(line);
+}
+
+var listTypos = new List<string>();
+foreach (var word in words)
+{
+    if (list.IndexOf(word) == -1)
+    {
+        listTypos.Add(word);
+    }
+}
+
+if (listTypos.Count > 0)
+{
+    Console.WriteLine("Looks like you have typos in next words: ");
+    foreach (var wordTypo in listTypos )
+    {
+        Console.WriteLine(wordTypo);
+    }
+}
